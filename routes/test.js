8d3ca7ee8323540b1486d0d08dbef9c21561a5e0
anto-d88 <antonio-123@hotmail.be>
2024-06-24 22:56:56 +0200

    first commit

--- routes/test.js
+++ routes/test.js
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../public/main.css">
+    <link rel="stylesheet" href="./main.css">
     <title>My E-Commerce</title>
 
   <link rel="stylesheet" href="./main.css">
