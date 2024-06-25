7b9d4d5c26aa2765f3f1c72a37de762dc09d9f7f
anto-d88 <antonio-123@hotmail.be>
2024-06-26 00:11:31 +0200

    first commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });*/
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'img', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'img', 'imgmaquette01.jpg'));
   });
 
 //app.use(accueilRoutes);
