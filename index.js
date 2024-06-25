fa5c0699b8237baa35d81ae5d74e2c756c564a90
anto-d88 <antonio-123@hotmail.be>
2024-06-26 00:06:43 +0200

    first commit

--- index.js
+++ index.js
@@ -9,12 +9,15 @@ app.use(express.static('public'));
 //app.set('view engine', 'ejs');
 //app.set('views', './views');
 // Route de base
-const accueilRoutes = require('./routes/test');
-/*app.get('/', (req, res) => {
+//const accueilRoutes = require('./routes/test');
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });*/
+  });
+app.get('/img/imgmaquette01.jpg', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'img', 'index.html'));
+  });
 
-app.use(accueilRoutes);
+//app.use(accueilRoutes);
 // Route About
 /*app.get('/about', (req, res) => {
     res.send('About Page');
