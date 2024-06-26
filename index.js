24708219ca5a6e2e8ca6ae7041fca7b18a36e78a
anto-d88 <antonio-123@hotmail.be>
2024-06-26 21:28:02 +0200

    commit

--- index.js
+++ index.js
@@ -7,26 +7,23 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-// Route de base
 app.use(express.static(path.join(__dirname, 'public')));
-//const accueilRoutes = require('./routes/test');
+// Route de base
+
+
+app.get('/img/imgmaquette01.jpg', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'img', 'imgmaquette01.jpg'));
+});
+
 app.get('/', (req, res) => {
-    res.render('indextest')
-    //res.sendFile(path.join(__dirname, 'public', 'index.ejs'));
-    });
-    
-    app.get('/img/imgmaquette01.jpg', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'img', 'imgmaquette01.jpg'));
-    });
- 
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public','main.css'));
-    });
-   
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'main.js'));
-    });
-    //   app.use(accueilRoutes);
+    res.sendFile(path.join(__dirname, 'public','main.css'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'main.js'));
+});
+const accueilRoutes = require('./routes/test');
+app.use(accueilRoutes);
 // Route About
 /*app.get('/about', (req, res) => {
     res.send('About Page');
