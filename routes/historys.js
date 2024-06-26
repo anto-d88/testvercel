fe81c77b066811fcb9dc1348501755b2985e015b
anto-d88 <antonio-123@hotmail.be>
2024-06-26 22:18:28 +0200

    first commit

--- routes/historys.js
+++ routes/historys.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const connection = require('../db');
+//const connection = require('../db');
 
 router.get('/history', (req, res) => {
    /* connection.query('SELECT * FROM products', (err, results) => {
