83257867a7f1fffdae992b2d51e44d7e726eda4c
anto-d88 <antonio-123@hotmail.be>
2024-06-28 20:37:03 +0200

    first commit

--- routes/product.js
+++ routes/product.js
@@ -4,25 +4,28 @@ const router = express.Router();
 const { createClient } = require('@supabase/supabase-js');
 const { Pool } = require('pg');
 
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
+
 const supabaseURL = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseURL, supabaseKey);
 
 const pool = new Pool({ connectionString: process.env.SUPABASE_BD_URL});
 
-router.use(express.urlencoded({ extended: true }));
-router.use(express.json());
+
 
 router.get('/produis', async (req, res) => {
+  
   try {
     const { data: products, error } = await supabase
       .from('products')
       .select('*');
     if (error) throw error;
-    res.render('produis', { products , user: req.session.user});
   } catch (err) {
     res.status(500).send('Erreur de base de données');
   }
+  res.render('produis', { products , user: req.session.user});
 });
 
 router.get('/product/:id', async (req, res) => {
@@ -34,10 +37,10 @@ router.get('/product/:id', async (req, res) => {
       .eq('id', id)
       .single();
     if (error) throw error;
-    res.render('produis', { products , user: req.session.user});
   } catch (err) {
     res.status(500).send('Erreur de base de données');
   }
+  res.render('produis', { products , user: req.session.user});
   //res.json(results[0]);
 });
 
