782e0eddaad9a9d0cd13e49a4fa4fb7145623853
anto-d88 <antonio-123@hotmail.be>
2024-06-28 20:28:47 +0200

    first commit

--- routes/historys.js
+++ routes/historys.js
@@ -2,6 +2,8 @@ const express = require('express');
 //const path = require('path');
 const router = express.Router();
 //const connection = require('../db');
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
 
 router.get('/history', (req, res) => {
    
--- routes/product.js
+++ routes/product.js
@@ -3,11 +3,15 @@ const router = express.Router();
 //const connection = require('../db');
 const { createClient } = require('@supabase/supabase-js');
 const { Pool } = require('pg');
+
 const supabaseURL = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseURL, supabaseKey);
 
 const pool = new Pool({ connectionString: process.env.SUPABASE_BD_URL});
+
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
 
 router.get('/produis', async (req, res) => {
   try {
