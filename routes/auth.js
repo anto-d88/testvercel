b27e6d82fa6b6e71b011aba518ace15fd7ada3a3
anto-d88 <antonio-123@hotmail.be>
2024-06-28 14:51:58 +0200

    first commit

--- routes/auth.js
+++ routes/auth.js
@@ -17,13 +17,13 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
   /*const hashedPassword = await bcrypt.hash(password, 10);
   connection.query('INSERT INTO users (username, password) VALUES (?, ?)', [username, hashedPassword], (err, results) => {
     if (err) {
       return res.status(500).send('Erreur de requête SQL');
     }*/
-      const { users, error } = await supabase.auth.signUp({ username, password });
+      const { users, error } = await supabase.auth.signUp({ email, password });
       if (error) {
           return res.status(500).json({ error: error.message });
       }
@@ -36,7 +36,7 @@ router.get('/login', (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
   /*connection.query('SELECT * FROM users WHERE username = ?', [username], async (err, results) => {
     if (err) {
       return res.status(500).send('Erreur de requête SQL');
@@ -52,7 +52,7 @@ router.post('/login', async (req, res) => {
         res.status(400).send('Mot de passe incorrect');
     }
     });*/
-    const { users, session, error } = await supabase.auth.signIn({ username, password });
+    const { users, session, error } = await supabase.auth.signIn({ email, password });
     if (error) {
         return res.status(500).json({ error: error.message });
     }
