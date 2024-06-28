5cd5f57985852d30d58072a3565ca6186a0d6aa7
anto-d88 <antonio-123@hotmail.be>
2024-06-28 15:52:03 +0200

    first commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 //const bcrypt = require('bcrypt');
 //const connection = require('../db');
 // configuration connection postGreSQL
