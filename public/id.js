e9c99e6d6de5dc6234c342b587698ed634983979
anto-d88 <antonio-123@hotmail.be>
2024-06-30 11:45:03 +0200

    first commit

--- public/id.js
+++ public/id.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hidenid = document.getElementById('hidenid').innerHTML;
-    
     const userId = hidenid ;
     const userLinkhistory = document.getElementById('history');
     userLinkhistory.href = `/history?userId=${encodeURIComponent(userId)}`;
