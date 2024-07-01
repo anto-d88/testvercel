7ea2d9254da959c163ef69c30d4410603d176192
anto-d88 <antonio-123@hotmail.be>
2024-07-01 11:52:30 +0200

    first commit

--- routes/product.js
+++ routes/product.js
@@ -23,21 +23,32 @@ const authenticate = (req, res, next) => {
 
 router.get('/produis', async (req, res) => {
  
+  const userId = req.query.userId;
+  const iduser = Number(userId);
+  let { data: users, error } = await supabase
+  .from('users')
+  .select('*')
+  .eq('id', iduser)
+if (error) {
+        return res.status(500).json({ error: error.message });
+    }
   try {
     const { data: products, error } = await supabase
       .from('products')
       .select('*');
     if (error) throw error;
-    res.render('produis', { products: products });
+    res.render('produis', { products: products, user: users[0].username });
   } catch (err) {
     res.status(500).send('Erreur de base de données');
   }
 });
 
 router.get('/product/:id',async (req, res) => {
-
- 
   const { id } = req.params;
+
+if (error) {
+        return res.status(500).json({ error: error.message });
+    }
   try {
     const { data: products, error } = await supabase
       .from('products')
