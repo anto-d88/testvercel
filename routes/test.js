ee9ff36b6b15111868adec497c320850862b4193
anto-d88 <antonio-123@hotmail.be>
2024-07-01 12:51:51 +0200

    first commit

--- routes/test.js
+++ routes/test.js
@@ -24,11 +24,30 @@ const authenticate = (req, res, next) => {
   next();
 };
 
-router.get('/indextest', (req, res) => {
+/*router.get('/indextest', (req, res) => {
   const username = req.session.user;
   console.log(username)
   
    res.render('indextest', { user: username })
-    });
- 
+    });*/
+    router.get('/indextest', async (req, res) => {
+
+      const userId = req.query.userId;
+      const id = Number(userId);
+      let { data: users, error } = await supabase
+      .from('users')
+      .select('*')
+      .eq('id', id)
+      
+    if (error) {
+            return res.status(500).json({ error: error.message });
+        }
+        console.log(users[0])
+       
+        //const match = await bcrypt.compare(password, user.password);
+        //if (password !== user.password) return res.status(400).send('Mot de passe incorrect'); 
+         
+      
+        res.render('indextest', { user: users[0] }); 
+          });
 module.exports = router;
