59cfa40b948fceb36b7f9f1f9dea5776ab4690f2
anto-d88 <antonio-123@hotmail.be>
2024-07-01 11:30:35 +0200

    first commit

--- routes/historys.js
+++ routes/historys.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const supabaseURL = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseURL, supabaseKey);
+
+const pool = new Pool({ connectionString: process.env.SUPABASE_BD_URL});
 router.use(express.urlencoded({ extended: true }));
 router.use(express.json());
 
@@ -12,8 +17,20 @@ router.use(express.json());
 router.get('/history', (req, res) => {
 
   const userId = req.query.userId;
+  let { data: users, error } = await supabase
+  .from('users')
+  .select('*')
+  .eq('id', userId)
   
-    res.render('history', { user: userId }); 
+if (error) {
+        return res.status(500).json({ error: error.message });
+    }
+    const user = users[0];
+    //const match = await bcrypt.compare(password, user.password);
+    //if (password !== user.password) return res.status(400).send('Mot de passe incorrect'); 
+     
+  
+    res.render('history', { user: users.username }); 
       });
   //});
 
