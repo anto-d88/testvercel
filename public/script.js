a18000afabca8f4195cc8ec50df8664e7afccfb3
anto-d88 <antonio-123@hotmail.be>
2024-07-19 17:45:37 +0200

    first commit

--- public/script.js
+++ public/script.js
@@ -1,39 +1,109 @@
 const cart = [];
 
 function addToCart(productId) {
+
     fetch(`/product/${productId}`)
         .then(response => response.json())
         .then(product => {
             const cartItem = cart.find(item => item.id === product.id);
+        
+           
             if (cartItem) {
                 cartItem.quantity++;
             } else {
                 cart.push({ ...product, quantity: 1 });
             }
             displayCart();
+            
         })
         .catch(error => console.error('Erreur de récupération du produit:', error));
-}
+        
+        
+    }
+    
+    function displayCart() {  
+        
+       
+        const supprim =  document.getElementById("deletarticl"); 
+        
+     
 
-function displayCart() {
-    const cartDiv = document.getElementById('cart');
-    cartDiv.innerHTML = '';
-    cart.forEach(product => {
+        const order = document.getElementById("orderButton")
+        
+        const cartDiv = document.getElementById('cart');
+        cartDiv.innerHTML = ''; 
+        cart.forEach(product => {
         const div = document.createElement('div');
-        div.textContent = `${product.name} - ${product.price}€ (Quantité: ${product.quantity})`;
+        div.setAttribute("id","articlajouter");
+        const prix =  `${product.price}`;
+        const quantity = `${product.quantity}`;
+        const total = document.createElement('div');
+        total.setAttribute("id","total");
+        if(product.quantity < 1){
+            div.textContent == '';
+            total.textContent == '';
+            supprim.style.display = "none";
+            order.style.display = "none"; 
+        }else{
+            div.textContent = `${product.name} - ${prix}€ (Quantité: ${quantity})`;
+            total.textContent ="Total:"+ prix * quantity + "€";
+            supprim.style.display = "block";
+            order.style.display = "block"; 
+        }
+        
         cartDiv.appendChild(div);
-    });
+        cartDiv.appendChild(total);
 
-    const orderButton = document.createElement('button');
-    orderButton.textContent = 'Passer commande';
-    const redirectioncommande = document.createElement('a');
-    redirectioncommande.setAttribute("href","/success"); 
-    orderButton.onclick = placeOrder;
-    cartDiv.appendChild(redirectioncommande);
-    redirectioncommande.appendChild(orderButton);
-}
+        const btnajout = document.getElementById("btnajout");
+
+        const deletarticl = document.getElementById("deletarticl");
+        deletarticl.addEventListener("click", function(){
+            div.textCocntent = `${product.name} - ${prix}€ (Quantité: ${quantity})`;
+
+            
+           
+        })
+        
+    });
+    
+    
+    
+    
+    
+    
+    
+} 
+function deletarticl(productId) {
+
+    fetch(`/product/${productId}`)
+        .then(response => response.json())
+        .then(product => {
+            const cartItem = cart.find(item => item.id === product.id);
+          
+           
+                if(cartItem.quantity > 0){
+                    cartItem.quantity--;
+                    
+                }
+               
+            
+            displayCart();
+            
+        })
+        .catch(error => console.error('Erreur de récupération du produit:', error));
+
+        
+    }
+
+
+
+/*function deletarticl(){
+    alert("hello");
+
+}*/
 
 function placeOrder() {
+    alert("hello")
     fetch('/success', {
         method: 'POST',
         headers: {
@@ -52,5 +122,7 @@ function placeOrder() {
     })
     .catch(error => console.error('Erreur lors de la commande:', error));
 }
+
+
 
 document.addEventListener('DOMContentLoaded', displayCart);
